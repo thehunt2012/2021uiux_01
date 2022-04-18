4a3e5b1da3d3bc2a6a7f1e0280b6144d5e6e979f
thehunt2012 <thehunt2012@naver.com>
2022-04-18 18:29:05 +0900

    20220418

--- 00웹디자인기능사/js/script02.js
+++ 00웹디자인기능사/js/script02.js
@@ -7,3 +7,34 @@ $(".mainmenu > li").mouseout(function(){
     $(".mainmenu > li > a").css("background","initial")
     $(".submenu").stop().slideUp(100);
 })
+
+// slide
+let num = 0;
+function slideUp(){
+    num++;
+    if(num == 4){
+        num = 1;
+        $(".sliderWrap").css("top",0)
+    }
+    $(".sliderWrap").animate({"top":-100*num + "%"},1000)
+}
+// setInterval(slideUp,3000)
+
+// tab menu
+$(".tab > div").click(function(e){
+    e.preventDefault();
+    let i = $(this).index();
+    $(".tab > div").removeClass("active")
+    $(this).addClass("active")
+
+    $(".content > ul").hide().eq(i).fadeIn();
+})
+
+// popup
+$(".popup").click(function(e){
+    e.preventDefault();
+    $(".pop_background").fadeIn();
+});
+$(".close").click(function(){
+    $(".pop_background").fadeOut();
+})
